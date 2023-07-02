a4190c4cb75b650d48458518b325f622dfc2f885
Ali Ashraf <95376706+AliAshraf0x1@users.noreply.github.com>
2023-07-02 03:08:07 +0300

    Add files via upload

---
+++ 1D Array.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+int *ptr;
+int n=0;
+int sum=0;
+int main() {
+    scanf("%d",&n);
+ptr=(int*)malloc( n*sizeof(int));
+        for(int x=0;x<n;x++)
+        {
+            scanf("%d",ptr+x);
+            sum +=*(ptr+x);
+            
+        }
+        printf("%d\n",sum);
+        free(ptr);
+    return 0;
+}
